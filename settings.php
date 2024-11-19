2ea654fd998bd28171a7a8de30d44c29dd8c74c8
Nyhr <35647907+nyhr@users.noreply.github.com>
2024-11-19 03:21:43 -0600

    Update settings.php
    Updated error handling

--- settings.php
+++ settings.php
@@ -1,15 +1,24 @@
 <?php
 
-ini_set('display_errors', parse_ini_file(__DIR__ . "/settings.ini")["error_logging"]["display_errors"]);
-ini_set('log_errors', parse_ini_file(__DIR__ . "/settings.ini")["error_logging"]["log_errors"]);
-ini_set('error_log', __DIR__ . "/" . parse_ini_file(__DIR__ . "/settings.ini")["error_logging"]["error_log"]);
+$settings = parse_ini_file(__DIR__ . "/settings.ini");
+$isDevelopment = $settings["mode"] === "development";
+
+if($isDevelopment) {
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', 0);
+    error_reporting(0);
+}
+
+ini_set('log_errors', $settings["log_errors"]);
+ini_set('error_log', $settings["error_log"]);
 
 if(php_sapi_name() !== "cli") {
     http_response_code(403);
     echo json_encode(["message" => "Forbidden"]);
     exit;
 }
-
 
 require_once(__DIR__ . "/src/controllers/UserController.php");
 require_once(__DIR__ . "/src/lib/database/index.php");
@@ -54,6 +63,13 @@ $roleTable->create("roles")
 ->execute();
 
 echo nl2br("Roles table created successfully \n");
+
+$userController = new UserController($db);
+if($userController->getByUsername("admin") === null) {
+    $userController->create(new UserModel(0,"admin", "admin@admin.com", "administrator", 3));
+
+    echo nl2br("Admin user created successfully \n");
+}
 
 echo nl2br("Database created successfully \n");
 ?>
